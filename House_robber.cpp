8b4faecc4b4e54ff96c3653bb0c0c35993d399a8
Manish Kumar <Manish.123jbad@gmail.com>
2021-12-11 19:40:08 +0530

    Create House_robber.cpp

---
+++ House_robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1){
+            return nums[0];
+        }
+        if(n == 2){
+            return max(nums[0], nums[1]);
+        }
+        int dp[n+1];
+        dp[0] = nums[0];
+        dp[1] = nums[1];
+        dp[2] = nums[2] + dp[0];
+        for(int i = 3; i < n; i++){
+            dp[i] = max(dp[i-3]+nums[i], nums[i] + dp[i-2]);
+        }
+        return max(dp[n-1], dp[n-2]);
+    }
+};
