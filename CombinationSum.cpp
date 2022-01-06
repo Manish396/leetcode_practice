699664d7fa39eb20cfa31d054c236bd6a1ebcc25
Manish Kumar <Manish.123jbad@gmail.com>
2022-01-06 22:51:15 +0530

    Create CombinationSum.cpp

---
+++ CombinationSum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector <vector <vector <int>>> dp(target+1);
+        dp[0]={{}};
+        for(int i:candidates)
+        {
+            for(int j=i;j<=target;j++)
+            {
+                for(auto v:dp[j-i])
+                {
+                    v.push_back(i);
+                    dp[j].push_back(v);
+                }
+            }
+        }
+        return dp[target];
+    }
+};
