77a35e7c7963739b44b58890c917d7e62ae5e55f
Manish Kumar <Manish.123jbad@gmail.com>
2022-02-07 20:50:20 +0530

    Update nextPermutation.cpp

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int start = n-2;
+        while(start >= 0){
+            if(nums[start] < nums[start + 1]){
+                break;
+            }
+            start--;
+        }
+        if(start < 0){
+            reverse(nums.begin(), nums.end());
+        }
+        else{
+            int k = n-1;
+            while(k > start){
+                if(nums[k] > nums[start]){
+                    break;
+                }
+                k--;
+            }
+            swap(nums[start], nums[k]);
+            reverse(nums.begin() + start + 1, nums.end());
+        }
     }
 };
